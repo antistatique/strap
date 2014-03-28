cfbc9f3c227b42fd1238e60b64817d23d854de0c
gagarine <simon@antistatique.net>
2014-03-28 18:50:22 +0100

    gulp build in build folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,46 +13,37 @@ var gulp = require('gulp'),
 
 // JS task
 gulp.task('scripts', function() {
-  gulp.src('./assets/js/*.js')
+  gulp.src('./assets/js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./assets/js/build'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('assets/js/build'));
+    .pipe(gulp.dest('./build/js'));
 });
 
 // JS vendors concat and minify
-gulp.task('js_vendors', function() {
+gulp.task('scripts_vendors', function() {
   gulp.src([
-      './vendors/**/*.js'
+      './vendors/bootstrap-sass/js/*.js'
     ])
     .pipe(concat('vendors.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('assets/js/build'));
+    .pipe(gulp.dest('./build/js'));
 });
 
 // SASS compile, autoprefix and minify task
 gulp.task('styles', function() {
-  return gulp.src('./assets/sass/brouillon.scss')
+  return gulp.src('./assets/sass/strap.scss')
     .pipe(sass())
     .on('error', gutil.beep)
     .on('error', notify.onError("Error: <%= error.message %>"))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(minifycss())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./build/css'));
 });
 
-// CSS vendors concat and minify
-gulp.task('css_vendors', function() {
-  gulp.src([
-      './vendors/tooltipster/css/tooltipster.css'
-    ])
-    .pipe(concat('vendors.css'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./assets/css'))
-});
 
 gulp.task('browser-sync', function() {
   browserSync.init(['./assets/css/*.css', '*.php', './assets/js/*.js'], {
@@ -60,7 +51,7 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('default', ['scripts', 'styles', 'css_vendors', 'js_vendors', 'watch', 'browser-sync']);
+gulp.task('default', ['scripts', 'styles', 'watch', 'browser-sync']);
 
 gulp.task('watch', function() {
   gulp.watch('./assets/sass/*.scss', ['styles']);
